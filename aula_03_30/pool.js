7369abaacd779d39dab11a9b630ea5c7404fa31d
ProfCarmino <prof.carmino@gmail.com>
2023-03-31 07:49:24 -0300

    final_da_aula_30_03

---
+++ aula_03_30/pool.js
@@ -0,0 +1,14 @@
+import mysql from "mysql2";
+
+const pool = mysql.createPool({
+    host: "127.0.0.1",
+    user: "root",
+    
+    database: "receitas",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+
+});
+
+export default pool;

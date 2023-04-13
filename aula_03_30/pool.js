c08be4121b5891e17cd7fcbeb36127fa8cda8c35
ProfCarmino <prof.carmino@gmail.com>
2023-04-13 16:56:12 -0300

    Login (06/04/2023)
    Finalizado login

--- aula_03_30/pool.js
+++ aula_03_30/pool.js
@@ -3,7 +3,6 @@ import mysql from "mysql2";
 const pool = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
-    
     database: "receitas",
     waitForConnections: true,
     connectionLimit: 10,
--- aula_03_30/routes/login.js
+++ aula_03_30/routes/login.js
@@ -14,4 +14,91 @@ routes.get("/login", (req, res, error) => {
     });
 });
 
+routes.get("/login/:id", (req, res, error) => {
+    const sql = 'SELECT * FROM login WHERE login.id_login=' + req.params.id;
+    pool.query(sql, (error, results, fields ) => {
+        if(!error){
+            if(results.length <= 0 ){
+                res.status(404).json({msg: "registro n~ao encontrado"})
+            } else {
+                res.status(200).json(results);
+            }
+        } else {
+            res.status(404).json({msg: "sem dados"});
+        }
+    })
+});
+
+routes.get("/login/email/:email", (req, res, error) => {
+    const sql = 'SELECT * FROM login WHERE login.email=?';
+    console.log("antes", sql); 
+    pool.query(sql, [email], (error, results, fields ) => {
+        if(!error){
+            res.status(200).json(results);
+        } else {
+            res.status(404).json({msg: "sem dados"});
+        }
+    })
+})
+
+routes.post("/login", (req, res, error) => {
+    const sql = 'INSERT INTO login( email, senha, nome, tipoUsuario ) ' +
+                'VALUES           (     ?,     ?,    ?,           ? )' 
+    const {email, senha, nome, tipoUsuario} = req.body; 
+    /*
+    const email = req.body.email; 
+    const senha = req.body.senha;
+    const nome = req.body.nome; 
+    const tipoUsuario = req.body.tipoUsuario;
+    */
+    pool.query(sql, [email, senha, nome, tipoUsuario], (error, results, fields ) => {
+        if(!error){
+            res.status(200).json(results);
+        } else {
+            console.log(error)
+            res.status(404).json({msg: "login cadastrado"});
+        }
+    })
+});
+
+routes.put("/login", (req, res, error) => {
+    const sql = 'UPDATE login SET email=?, senha=?, nome=?, tipoUsuario=?  WHERE id_login=?'
+    const {email, senha, nome, tipoUsuario, id_login} = req.body;
+    pool.query(sql, [email, senha, nome, tipoUsuario, id_login], (error, results, fields ) => {
+        if(!error){
+            res.status(200).json(results);
+        } else {
+            console.log(error)
+            res.status(404).json({msg: error});
+        }
+    })
+} )
+
+routes.put("/login/id/:id", (req, res, error) => {
+    const sql = 'UPDATE login SET email=?, senha=?, nome=?, tipoUsuario=?  WHERE id_login=?'
+    const {email, senha, nome, tipoUsuario} = req.body;
+    const id_login = req.params.id; 
+    pool.query(sql, [email, senha, nome, tipoUsuario, id_login], (error, results, fields ) => {
+        if(!error){
+            res.status(200).json(results);
+        } else {
+            console.log(error)
+            res.status(404).json({msg: error});
+        }
+    })
+} );
+
+routes.delete("/login/:id", (req, res, error) => {
+    const sql = 'DELETE FROM login WHERE id_login=?'
+    const id_login = req.params.id; 
+    pool.query(sql, [id_login], (error, results, fields ) => {
+        if(!error){
+            res.status(200).json(results);
+        } else {
+            console.log(error)
+            res.status(404).json({msg: error});
+        }
+    });
+});
+
 export default routes;
